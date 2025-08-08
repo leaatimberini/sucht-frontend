d0fe670d041b3f7be7f4e89ef1173ecc93e6e933
Leandro <timberinil@gmail.com>
2025-08-08 19:19:26 -0300

    Version 3.0.7 Frontend

--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -6,7 +6,6 @@ import api from '@/lib/axios';
 import { Instagram, Download, Link as LinkIcon, CheckCircle, X } from 'lucide-react';
 import { useAuthStore } from '@/stores/auth-store';
 
-
 export function ShareButton({ eventId, eventTitle, flyerImageUrl }: { eventId: string, eventTitle: string, flyerImageUrl: string | null }) {
   const [isMobile, setIsMobile] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -122,5 +121,4 @@ export function ShareButton({ eventId, eventTitle, flyerImageUrl }: { eventId: s
       )}
     </>
   );
-
 }
